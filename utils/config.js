5fc10a7de32dcd743370fb02e5366bdc402e95f7
Vitaliy Keretsman <vitalyacode@gmail.com>
2021-11-09 20:14:30 +0200

    a

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 
 const MONGODB_URI = process.env.NODE_MODE === 'production'
   ? process.env.MONGODB_URI
